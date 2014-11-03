640145d27663dd8f42b74e547127ee9df06f2933
Jing@GoodLuck_Jing <junfind@hotmail.com>
2014-11-03 21:24:10 +0800

    添加GUI

---
+++ Demo1/Assets/Scritps/TestGUIScripts.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestGUIScripts : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+    void OnGUI()
+    {
+        GUI.Box(new Rect(Screen.width / 2 - 100f, Screen.height / 2 - 50f, 200, 100), "Good Luck Jing");
+    }
+}
